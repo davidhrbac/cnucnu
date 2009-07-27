319fe7c4170f35d28ff726b6969aefcfe5bc392d
Till Maas <opensource@till.name>
2009-07-27 08:03:30 +0200

    query release from repository

--- cnucnu/package_list.py
+++ cnucnu/package_list.py
@@ -162,32 +162,39 @@ class Repository:
         self.repofrompath = "%s,%s" % (self.repoid, self.path)
 
         self.package_list = package_list
-        self._package_version_list = None
+        self._nvr_dict = None
 
-    def package_version_list(self, package=None):
-        if not self._package_version_list or (package and package.name not in self._package_version_list):
+    def nvr_dict(self, package=None):
+        if not self._nvr_dict or (package and package.name not in self._nvr_dict):
             if package and package not in self.package_list:
                 self.package_list.packages.append(package)
             package_names = [p.name for p in self.package_list.packages]
-            self._package_version_list = self.repoquery(package_names)
+            self._nvr_dict = self.repoquery(package_names)
 
-        return self._package_version_list
+        return self._nvr_dict
 
     def repoquery(self, package_names=[]):
         import subprocess as sp
         # TODO: get rid of repofrompath message even with --quiet
-        cmdline = ["/usr/bin/repoquery", "--quiet", "--archlist=src", "--all", "--repoid", self.repoid, "--qf", "%{name}\t%{version}"]
+        cmdline = ["/usr/bin/repoquery", "--quiet", "--archlist=src", "--all", "--repoid", self.repoid, "--qf", "%{name}\t%{version}\t{release}"]
         if self.repofrompath:
             cmdline.extend(['--repofrompath', self.repofrompath])
         cmdline.extend(package_names)
 
         repoquery = sp.Popen(cmdline, stdout=sp.PIPE)
         (list, stderr) = repoquery.communicate()
-        return dict([e.split("\t") for e in list.split("\n") if e != ""])
-
+        new_nvr_dict = {}
+        for line in list.split("\n"):
+            if line != "":
+                name, version, release = line.split("\t")
+                new_nvr_dict[name] = (version, release)
+        return new_nvr_dict
 
     def package_version(self, package):
-        return self.package_version_list(package)[package.name]
+        return self.nvr_dict(package)[package.name][0]
+    
+    def package_release(self, package):
+        return self.nvr_dict(package)[package.name][1]
 
 
 class PackageList:
