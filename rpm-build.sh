8504009ddbe879d1c7fe2d5b9f3eb9699c4b8425
Till Maas <opensource@till.name>
2011-07-17 20:31:11 +0200

    fix rpm-build.sh: do not use stale data
    Clean the rpm build dir after building to avoid a conflict with setup.py
    sdist.

--- rpm-build.sh
+++ rpm-build.sh
@@ -2,7 +2,7 @@
 
 PATH="/bin:/usr/bin:/sbin:/usr/sbin:/home/till/bin"
 
+rm -- noarch/*
 ./setup.py sdist
 ln -s dist/cnucnu-0.0.0.tar.gz
-rpmbuild-thisdir.sh -bb cnucnu.spec
-rm cnucnu-0.0.0.tar.gz
+rpmbuild-thisdir.sh --rmsource --clean -bb cnucnu.spec
